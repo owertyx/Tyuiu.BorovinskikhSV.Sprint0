6dbc766f0702a78061fff70dc244c0c17d9ea473
BorovinskikhSV <popov@DESKTOP-SAAMN2L>
2026-09-11 22:37:11 +0500

    Task3

---
+++ Tyuiu.BorovinskikhSV.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.BorovinskikhSV.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.BorovinskikhSV.Sprint0.Task3.V0
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
